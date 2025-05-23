573b0cfb8879e731853c5b4ecaa46115c37f40cf
paulapc <ppena@unicauca.edu.co>
2025-05-23 01:04:22 -0500

    admin identificator prod

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   API_USERS_URL: 'http://localhost:8090/monitoring-tool-users-ms',
   API_TRACKING_URL: 'http://localhost:8092/monitoring-tool-tracking-ms',
   SOCKET_NOTIFICATION_URL: 'ws://localhost:8091/monitoring-tool-notification-ms/ws/notifications',
-  
+  ADMIN_IDENTIFICATOR : "ADMIN"
 };
