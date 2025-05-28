a8a87c8b31eaa9541930ab9a3bfb81ebfcd77be9
Julián Santiago Martinez Trullo <67648220+JulianSanti28@users.noreply.github.com>
2025-05-27 20:32:15 -0500

    Update environment.prod.ts

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  API_USERS_URL: 'http://ec2-18-212-179-114.compute-1.amazonaws.com:8090/monitoring-tool-users-ms',
-  API_TRACKING_URL: 'http://ec2-18-212-179-114.compute-1.amazonaws.com:8092/monitoring-tool-tracking-ms',
-  SOCKET_NOTIFICATION_URL: 'ws://http://ec2-18-212-179-114.compute-1.amazonaws.com:8091/monitoring-tool-notification-ms/ws/notifications',
+  API_USERS_URL: 'http://ec2-52-70-30-124.compute-1.amazonaws.com:8090/monitoring-tool-users-ms',
+  API_TRACKING_URL: 'http://ec2-52-70-30-124.compute-1.amazonaws.com:8092/monitoring-tool-tracking-ms',
+  SOCKET_NOTIFICATION_URL: 'ws://http://ec2-52-70-30-124.compute-1.amazonaws.com:8091/monitoring-tool-notification-ms/ws/notifications',
   
   ADMIN_IDENTIFICATOR : "ADMIN",
 
